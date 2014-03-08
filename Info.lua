9039bcd5e152d3c707cdaec4a8a5e8a8695a4019
STRWarrior <niels.breuker@hotmail.nl>
2014-03-08 19:48:50 +0100

    Added a small description in the Info.lua and added a Readme.md

--- Info.lua
+++ Info.lua
@@ -8,8 +8,9 @@ g_PluginInfo =
 	Name = "WorldUtilities",
 	Version = 1,
 	Date = "2014-3-8",
-	SourceLocation = "",
+	SourceLocation = "https://github.com/STRWarrior/WorldUtilities",
 	Description = [[
+	A plugin that allows you to manage the weather and time in your world.
 	]],
 	
 	Commands =
@@ -23,13 +24,13 @@ g_PluginInfo =
 			{
 				weather =
 				{
-					HelpString = "",
+					HelpString = "The command that manages the weather. As parameters it has: Set, Stop, Start and Change.",
 					Permission = "WorldUtilities.weather",
 					Handler = HandleWeatherCommand,
 				},
 				time =
 				{
-					HelpString = "",
+					HelpString = "The command that manages the time. As parameters it has: Set, Stop and Start.",
 					Permission = "WorldUtilities.time",
 					Handler = HandleTimeCommand,
 				},
---
+++ Readme.md
@@ -0,0 +1,17 @@
+A plugin that allows you to manage the weather and time in your world. 	
+
+# Commands
+
+### General
+| Command | Permission | Description | 
+| ------- | ---------- | ----------- | 
+|/wu time | WorldUtilities.time | The command that manages the time. As parameters it has: Set, Stop and Start.| 
+|/wu weather | WorldUtilities.weather | The command that manages the weather. As parameters it has: Set, Stop, Start and Change.| 
+
+
+
+# Permissions
+| Permissions | Description | Commands | Recommended groups |
+| ----------- | ----------- | -------- | ------------------ |
+WorldUtilities.time |  | `/wu time` | 
+WorldUtilities.weather |  | `/wu weather` | 
